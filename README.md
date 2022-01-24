9726b76ef67b656e2293f918b5a000e11595d3b4
IvanRamyk <ivan.r@mate.academy>
2022-01-24 21:55:17 +0200

    Add `py-feed-animals` task

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
-# Python boilerplate for GitHub tasks
+# Feed animals
 
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
-- Implement the task described [here](app/main.py)
+All animals love delicious food. Let's create a new class to feed them.
+Create class `Animal` which constructor takes three arguments:
+* `name` - the animal name
+* `appetite` - an integer that shows how much *food points* this animal need to eat to be full.
+* `is_hungry` - boolean that shows if animal is ready to eat with `True` default value.
+
+`Animal` should have two methods:
+* `print_name` - should print name in the following format: `Hello, my name is {name}`
+* `feed` - should print `Eating {appetite} food points...`, 
+set `is_hungry` to `False` and return number of eaten food points if animal is hungry.
+Otherwise, it should return 0.
+
+Example:
+```python
+lion = Animal("Lion", 25)
+lion.print_name()  # "Hello, I'm Lion"
+food_points = lion.feed()  # "Eating 25 food points..."
+print(food_points)  # 25
+print(lion.is_hungry)  # False
+print(lion.feed())  # 0
+```
+
+There is a well-known fact that all cats eat 3 food points at a time.
+Also, they can catch a mouse.
+Write `Cat` class which is a child of `Animal`. 
+It should have a constructor with two arguments:
+* `name` - the name of a cat
+* `is_hungry` - with `True` default value
+
+Note: you need call the super class constructor with `appetite` equal to 3.
+
+`Cat` should have only one additional method `catch_mouse` which should print 
+`The hunt began!`
+
+Example:
+```python
+cat = Cat("Cat")
+cat.print_name()  # "Hello, I'm Cat"
+cat.feed()  # "Eating 3 food points"
+
+cat2 = Cat("Cat", False)
+print(cat2.feed())  # 0
+cat2.catch_mouse()  # "The hunt began!"
+```
+
+The last class you should implement is a `Dog` class.
+Its constructor should have two arguments:
+* `name` - the name of a dog
+* `is_hungry` - with `True` default value
+
+All dogs should have `appetite` equal to 7.
+
+`Dog` should have one additional method `bring_slippers` that should print
+`The slippers delivered!`
+
+Example:
+```python
+dog = Dog("Dog")
+dog.print_name()  # "Hello, I'm Dog"
+dog.feed()  # "Eating 7 food points"
+
+dog2 = Dog("Dog", False)
+print(dog2.feed())  # 0
+dog2.bring_slippers()  # "The slippers delivered!"
+```
+
+Now, it's time to feed many animals at a time.
+Implement `feed_animals` function which takes a list of animals 
+and should return a sum of food points that is needed to feed all hungry
+animals from this list.
+
+Example:
+```python
+cat = Cat("Cat", False)
+lion = Animal("Lion", 25, True)
+dog = Dog("Dog")
+feed_animals([cat, lion, dog]) == 28
+```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,182 @@
-# TODO: add tests
-from app.main import hello_world
+import ast
+import inspect
+import io
+from contextlib import redirect_stdout
+
+import pytest
+
+import app.main as main
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+@pytest.mark.parametrize("class_name", ["Animal", "Cat", "Dog"])
+def test_classes_should_be_defined(class_name):
+    assert hasattr(main, class_name)
+
+
+@pytest.mark.parametrize(
+    "class_name,methods",
+    [
+        ("Animal", ["print_name", "feed"]),
+        ("Cat", ["print_name", "feed", "catch_mouse"]),
+        ("Dog", ["print_name", "feed", "bring_slippers"]),
+    ],
+)
+def test_classes_should_have_corresponding_methods(class_name, methods):
+    class_to_test = getattr(main, class_name)
+    for method in methods:
+        assert (
+            hasattr(class_to_test, method) is True
+        ), f"Class '{class_to_test}' should have method {method}"
+
+
+@pytest.mark.parametrize(
+    "class_name,method",
+    [
+        ("Cat", "catch_mouse"),
+        ("Dog", "bring_slippers"),
+    ],
+)
+def test_only_one_method_should_be_declared_in_each_of_children_classes(
+    class_name, method
+):
+    class_to_test = getattr(main, class_name)
+    class_source = inspect.getsource(class_to_test)
+    parsed_class = ast.parse(class_source)
+    assert [name.id for name in parsed_class.body[0].bases] == [
+        "Animal"
+    ], f"'{class_name}' should be inherited from 'Animal'"
+    assert (
+        len(parsed_class.body[0].body) == 2
+    ), f"Only one method '{method}' should be defined inside class '{class_name}' except '__init__'"
+    assert method in (
+        parsed_class.body[0].body[0].name,
+        parsed_class.body[0].body[1].name,
+    ), f"Only one method '{method}' should be defined inside class '{class_name}' except '__init__'"
+
+
+@pytest.mark.parametrize(
+    "name,appetite,output",
+    [
+        ("Puppy", 1, "Hello, I'm Puppy\n"),
+        ("Kitten", 10, "Hello, I'm Kitten\n"),
+    ],
+)
+def test_animal_print_name_method(name, appetite, output):
+    animal = main.Animal(name, appetite)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        animal.print_name()
+    assert f.getvalue() == output
+
+
+@pytest.mark.parametrize(
+    "name,appetite,output",
+    [
+        ("First", 3, "Eating 3 food points...\n"),
+        ("Second", 7, "Eating 7 food points...\n"),
+        ("Third", 102, "Eating 102 food points...\n"),
+    ],
+)
+def test_animal_feed_method(name, appetite, output):
+    animal = main.Animal(name, appetite)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        assert animal.feed() == appetite
+    assert f.getvalue() == output
+
+
+@pytest.mark.parametrize(
+    "name,output",
+    [
+        ("Cat1", "Hello, I'm Cat1\n"),
+        ("Cat2", "Hello, I'm Cat2\n"),
+        ("Cute Cat", "Hello, I'm Cute Cat\n"),
+        ("Lady Stark", "Hello, I'm Lady Stark\n"),
+    ],
+)
+def test_cat_print_name_method(name, output):
+    cat = main.Cat(name)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        cat.print_name()
+    assert f.getvalue() == output
+
+
+@pytest.mark.parametrize(
+    "name,output",
+    [
+        ("Cat1", "Eating 3 food points...\n"),
+        ("Cat2", "Eating 3 food points...\n"),
+        ("Cat3", "Eating 3 food points...\n"),
+    ],
+)
+def test_cat_feed_method(name, output):
+    cat = main.Cat(name)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        assert cat.feed() == 3
+    assert f.getvalue() == output
+
+
+@pytest.mark.parametrize(
+    "name,output",
+    [
+        ("Dog1", "Hello, I'm Dog1\n"),
+        ("Dog2", "Hello, I'm Dog2\n"),
+        ("Cute Dog", "Hello, I'm Cute Dog\n"),
+    ],
+)
+def test_dog_print_name_method(name, output):
+    dog = main.Dog(name)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        dog.print_name()
+    assert f.getvalue() == output
+
+
+@pytest.mark.parametrize(
+    "name,output",
+    [
+        ("Dog1", "Eating 7 food points...\n"),
+        ("Dog2", "Eating 7 food points...\n"),
+        ("Dog3", "Eating 7 food points...\n"),
+    ],
+)
+def test_dog_feed_method(name, output):
+    dog = main.Dog(name)
+    f = io.StringIO()
+    with redirect_stdout(f):
+        assert dog.feed() == 7
+    assert f.getvalue() == output
+
+
+def test_cat_catch_method():
+    cat = main.Cat("My cat")
+    f = io.StringIO()
+    with redirect_stdout(f):
+        cat.catch_mouse()
+    assert f.getvalue() == "The hunt began!\n"
+
+
+def test_dog_bring_slippers_method():
+    dog = main.Dog("My dog")
+    f = io.StringIO()
+    with redirect_stdout(f):
+        dog.bring_slippers()
+    assert f.getvalue() == "The slippers delivered!\n"
+
+
+@pytest.mark.parametrize(
+    "animals,total_food_points",
+    [
+        ([main.Cat("")], 3),
+        ([main.Cat("", False)], 0),
+        ([main.Dog("")], 7),
+        ([main.Animal("", 10, False), main.Animal("", 10, False)], 0),
+        ([main.Cat(""), main.Dog(""), main.Animal("", 10)], 20),
+        ([main.Cat(""), main.Cat(""), main.Cat("")], 9),
+        ([main.Dog(""), main.Dog(""), main.Dog(""), main.Animal("", 100, False)], 21),
+    ],
+)
+def test_feed_animals_function(animals, total_food_points):
+    assert main.feed_animals(animals) == total_food_points
